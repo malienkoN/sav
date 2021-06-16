3b0c3b528ceda5acb530dbfa9de017d47a3df27b
malienkoN <nekmalienko2001@gmail.com>
2021-06-16 15:25:00 +0300

    slider & header start

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-
+    const swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
 });
