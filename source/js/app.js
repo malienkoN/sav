f41b93dbaff4bd906e4eaa7381b92c1692f69c47
malienkoN <nekmalienko2001@gmail.com>
2021-06-29 09:12:29 +0300

    team

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,44 @@
 $(document).ready(function () {
-    const swiper = new Swiper(".mySwiper", {
+    new Swiper(".mySwiper", {
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
+        },
+    });
+
+    // accordion
+    $('.accordion__item-trigger').click(function () {
+        $(this).next('.accordion__item-content').slideToggle();
+        $(this).parent().toggleClass('active');
+        const replaceText = $(this).parent().hasClass('active') ? '-' : '+';
+        $(this).find('.accordion__item-trigger__plus').text(replaceText);
+    });
+
+    // $('.accordion__item-trigger:first').click();
+
+    new Swiper(".swiperNews", {
+        slidesPerView: 1.3,
+        spaceBetween: 1,
+        loop: true,
+        centeredSlides: true,
+        roundLengths: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 1.5,
+                spaceBetween: 1,
+            },
+            1280: {
+                slidesPerView: 3,
+                spaceBetween: 1,
+            },
         },
     });
 });
